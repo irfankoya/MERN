61177aa17caec57822dcc2b808900e17367f827b
Irfan koya <you@example.comgit config --global user.email>
2024-10-24 09:55:11 +0530

    Signin API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcrypt";
-
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,5 +12,24 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_CODE);
+    const {password: hashedPassword, ...rest} =validUser._doc //the _doc used to take only the needed details of the user
+    const expiryDate= new Date(Date.now()+3600000);//1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires:expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error)
   }
 };
